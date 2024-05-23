8385d607841d956a7826e4f804b1addb25ea8519
IrluiDev <iramirez.spain@gmail.com>
2024-05-23 21:21:07 +0200

    Mensaje descriptivo de tus cambios

--- __manifest__.py
+++ __manifest__.py
@@ -1,18 +1,16 @@
 {
-    'name': 'Custom Alda pms_propertyes Regions Management',
+    'name': 'Alda PMS HR Regions Management',
     'version': '14.0.1.0.0',
-    'category': 'HPMS/HR',
-    'summary': 'Manage pms_property regions and assign job positions to properties.',
-    'author': 'Irlui Ramirez,José Luis Algara,Odoo Community Association (OCA)',
-    'depends': [
-        'base',
-        'hr',
-        'pms',
-    ],
+    'category': 'PMS/HR',
+    "summary": """ Region and Staff associated with the property """,
+    "author": "Irlui Ramirez,José Luis Algara,Odoo Community Association (OCA)",
+    "depends": ["base", "pms", "hr"],
     'data': [
         'views/region_views.xml',
         'views/pms_property_region_assignment_views.xml',
     ],
-    'installable': True,
-    'application': False,
+    "application": True,
+    "installable": True,
+    "auto_install": False,
+    "license": "AGPL-3",
 }
